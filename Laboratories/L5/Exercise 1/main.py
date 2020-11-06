be0dfc944f966e72c40aaf7390e603ff192e0150
AndreaGhiglione <s247257@studenti.polito.it>
2020-11-06 18:10:05 +0100

    Ex 1.6 completed

--- Laboratories/L5/Exercise 1/main.py
+++ Laboratories/L5/Exercise 1/main.py
@@ -1,7 +1,12 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from ny_scatter import NYScatterPlot
+
+def get_id_cell(lon, lat, min_lon, max_lon, min_lat, max_lat, num_of_grids):
+    lat_grid = (lat - min_lat) / (max_lat - min_lat) * num_of_grids
+    lon_grid = (lon - min_lon) / (max_lon - min_lon) * num_of_grids
+    id_cell = lon_grid + lat_grid * num_of_grids
+    return id_cell
 
 df = pd.read_csv('pois_all_info',sep='\t',low_memory=False)
 municipality_pois = pd.read_csv('ny_municipality_pois_id.csv')
@@ -9,14 +14,11 @@ municipality_pois = pd.read_csv('ny_municipality_pois_id.csv')
 # I select the rows with id in ny_municipality through a mask
 mask_id = df['@id'].isin(list(municipality_pois.values))
 df = df[mask_id]
-print(df)
-
 
 print(f'Number of missing values over {len(df)} per column: ')
 for column in df.keys():
     num_missing_values = len(df.loc[df[column].isnull(),column])
     print(f'{column}: {num_missing_values}')
-
 
 categories = ['amenity','shop','public_transport','highway']
 threshold = 2  # percentage
@@ -33,8 +35,8 @@ for category in categories:
     tmp_df.columns = ['Occurrences']  # I give a name to the column
     tmp_df = tmp_df.sort_values(by=['Occurrences'],axis=0,ascending=False)
 
-    # tmp_df.plot(kind='bar',title=f'Occurences of {category}',grid=True,legend=False,figsize=(14,5),rot=0)
-    # plt.show()
+    tmp_df.plot(kind='bar',title=f'Occurences of {category}',grid=True,legend=False,figsize=(14,5),rot=0)
+    plt.show()
 
     frequent_data[category] = tmp_df
 
@@ -43,7 +45,17 @@ freq_data_shop = df[df['shop'].isin(frequent_data['shop'].index)]
 freq_data_public_transport = df[df['public_transport'].isin(frequent_data['public_transport'].index)]
 freq_data_highway = df[df['highway'].isin(frequent_data['highway'].index)]
 
-freq_poi = {'amenity': freq_data_amenity, 'shop': freq_data_shop, 'public_transport': freq_data_public_transport, 'highway': freq_data_highway}
+freq_data = pd.concat((freq_data_amenity,freq_data_shop,freq_data_public_transport,freq_data_highway))
+
+freq_poi = {'amenity': freq_data_amenity, 'shop': freq_data_shop, 'public_transport': freq_data_public_transport, 'highway': freq_data_highway}  # 'poi' = 'data'
 
 ny_scat_plot = NYScatterPlot()
 ny_scat_plot.scatter_plot(freq_poi,'New_York_City_Map.PNG')
+
+(min_lon,max_lon) = (ny_scat_plot.ext[0],ny_scat_plot.ext[1])
+(min_lat,max_lat) = (ny_scat_plot.ext[2],ny_scat_plot.ext[3])
+num_of_grids = 10
+freq_data["grid_id"] = get_id_cell(freq_data["@lon"], freq_data["@lat"], min_lon, max_lon, min_lat, max_lat, num_of_grids)
+freq_data["grid_id"] = freq_data["grid_id"].astype(int)
+
+print(freq_data.groupby(['grid_id']).count().drop(columns=['@id','@type','@lat','@lon','name']))
--- Laboratories/L5/Exercise 1/ny_scatter.py
+++ Laboratories/L5/Exercise 1/ny_scatter.py
@@ -1,11 +1,15 @@
 import matplotlib.pyplot as plt
 import imageio
-import numpy as np
 
 class NYScatterPlot:
     def __init__(self):
         self.poi = None
         self.img = None
+        self.min_lat = None
+        self.max_lat = None
+        self.min_lon = None
+        self.max_lon = None
+        self.ext = None
 
     def scatter_plot(self,poi,image):
         self.poi = poi
@@ -14,17 +18,18 @@ class NYScatterPlot:
 
         zor = 1
         for category in poi.keys():
-            plt.scatter(x=poi[category]['@lon'],y=poi[category]['@lat'],zorder=zor,marker='.',c=colors[category],label=category,alpha=0.5*1/zor)
+            plt.scatter(x=poi[category]['@lon'],y=poi[category]['@lat'],zorder=zor,marker='.',c=colors[category],label=category,alpha=0.5/zor)
             zor += 1
 
+        plt.grid()
         plt.legend(title='NY point of interests', loc='upper left')
         axes = plt.gca()
         x_lim_scatter = axes.get_xlim()
         y_lim_scatter = axes.get_ylim()
-        ext = [x_lim_scatter[0], x_lim_scatter[1], y_lim_scatter[0], y_lim_scatter[1]]
+        self.ext = [x_lim_scatter[0], x_lim_scatter[1], y_lim_scatter[0], y_lim_scatter[1]]
         img = plt.imread("New_York_City_Map.PNG")
-        plt.imshow(img, zorder=0, extent=ext)
+        plt.imshow(img, zorder=0, extent=self.ext)
 
-        aspect = img.shape[0] / float(img.shape[1]) * ((ext[1] - ext[0]) / (ext[3] - ext[2]))
+        aspect = img.shape[0] / float(img.shape[1]) * ((self.ext[1] - self.ext[0]) / (self.ext[3] - self.ext[2]))
         plt.gca().set_aspect(aspect)
         plt.show()
